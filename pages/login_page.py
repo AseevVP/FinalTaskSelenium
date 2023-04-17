49e15e1c2a235ddc3020c653071ac7fb589b8595
aseevvp <aseev_vp86@gmail.com>
2023-04-17 12:11:34 +0300

    login page locators for login page tests for checking login and register forms

---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        self.browser.find_element(*LoginPageLocators.LOGIN_LINK)
+        assert True, 'There is no Login link'
+
+    def should_be_login_form(self):
+        self.browser.find_element(*LoginPageLocators.LOGIN_LINK).click()
+        header = self.browser.find_element(*LoginPageLocators.LOGIN_FORM).text
+        print(header)
+        assert header == 'Войти', 'The Login form is unavailable'
+
+    def should_be_register_form(self):
+        self.browser.find_element(*LoginPageLocators.LOGIN_LINK).click()
+        header = self.browser.find_element(*LoginPageLocators.REGISTRATION_FORM).text
+        print(header)
+        assert header == 'Зарегистрироваться', 'The Registration form is unavailable'
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 
@@ -16,4 +17,19 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 
+def test_should_be_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
 
+
+def test_should_be_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, link)
+    login_page.should_be_register_form()
